0984e203a71a3a11e455e2d98a1527042f7dc78c
annul27 <samairaansari006.sa@gmail.com>
2025-05-11 11:19:40 +0530

    Add files via upload

---
+++ video 54/script.js
@@ -0,0 +1,17 @@
+alert("Hello World!")
+
+console.log("Code is running...."); 
+console.log("Code is also running...."); 
+console.log("Code is running like a wow...."); 
+
+var a = prompt("Enter a number: ");
+var isTrue = confirm("Are you sure you want to leave this page and blast your computer? ");
+
+if (isTrue) {
+    console.log("Computer is blasting....");
+}
+
+else {
+    console.log("Computer is safe....");
+}
+console.log("The number is: " + a);
